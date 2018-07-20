5954f945f95e74726f1b8fba62803d880b448f14
chefgonzo <chefgonzo@gmail.com>
2018-07-19 21:52:44 -0400

    stuck on JS

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -37,47 +37,56 @@ var losses = 0;
 var guessesLeft = 10;
 var wrongGuesses = [];
 var correctGuesses = [];
+var bank = [];
 
-//Start the game, reset counters, randomly select a word, then show the #of possible letters
-function startGame(){
+//Reset and start over
+function startOver(){
+    guessesLeft = 10;
+    document.querySelector("#remain").innerHTML = guessesLeft;
+    correctGuesses = [];
+    incorrectGuesses = [];
+    document.querySelector("#guess").innerHTML = "";
+    bank = [];
+    wordPick();
+    solutionSplit();
+    visualAnswer();
+}
+window.onload = startOver;
+console.log("startOver Fired")
+
+//selects the word
+function wordPick(){
     // Randomly Select A Word
     solutionPicked = solutions[Math.floor(Math.random() * solutions.length)];
     console.log(solutionPicked);
-    //reset all applicable sections - everything except wins and losses
-    guessesLeft = 10;
-    wrongGuesses = [];
-    dashes = [];
-    rightGuesses = 0;
-    document.querySelector("#remain").innerHTML = guessesLeft;
-    // Split the solution into individual characters and draw dashes
-    solutionSplit = solutionPicked.split("");
-    console.log(solutionSplit);    
-    for (var i = 0; i< solutionSplit.length; i++){
-        if (solutionSplit[i].indexOf(" ")!=-1){
-            dashes.push(" ")
-        }
-        else {
-        dashes.push("_ ");
-        }
-        document.querySelector("#currentWord").innerHTML = dashes;
-        
-    }
-    
+    return solutionPicked;    
 }
-window.onload = startGame;
+//simply defining solutionPicked outside of the function
+var solutionPicked = wordPick();
+
+//Split the answer and store it
+function solutionSplit(){
+    for (i = 0; i<solutionPicked.length; i++){
+        bank.push(solutionPicked[i]);
+        console.log(bank);
+    }
+} 
+
+//Adds the underscores for the solution to the page
+function visualAnswer(){
+    var underScores = indexOf(solutionPicked);
+    document.querySelector("#currentWord").innerHTML = underScores;
+    underScores.push("_ ");
+}
 
 // player picks a letter
-document.onkeypress = function(event){
+document.onkeyup = function(event){
     var letterPicked = event.key;
     letterPicked.toLowerCase();
+    console.log("The keypressed was: " + letterPicked);
 }
 
 //check the letter against the solution
-function checkLetter(letterPicked){
-    if (letterPicked === solutionSplit[i]){
-        dashes.push("letterPicked")
-    }
-}
 // if the letter is right show it in the solution
 //if its wrong show it as wrong and remove a guess
 // if they run out of guesses -- game over
